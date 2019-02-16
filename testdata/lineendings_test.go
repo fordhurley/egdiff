303f9248797a61f2ba8aeb675408f70d0597f3e9
Ford Hurley <ford.hurley@gmail.com>
2019-02-16 08:59:07 -0500

    Move into _test file

--- testdata/lineendings_test.go
+++ testdata/lineendings_test.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
+var lineEndingRE = regexp.MustCompile(`\r?\n`)
+
+// replaceLineEndings changes \r\n to \n. It's idempotent.
+func replaceLineEndings(raw string) string {
+	return lineEndingRE.ReplaceAllString(raw, "\n")
+}
 func Example_replaceLineEndings() {
 	fmt.Printf("%#v\n", replaceLineEndings("a\r\n\r\nb\r\n\r\nc"))
 	fmt.Printf("%#v\n", replaceLineEndings("a\r\nb\r\nc"))
